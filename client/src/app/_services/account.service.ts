0d2d55732c18326a125e2ba7ceb99906baf36cf9
worawut-kr <worawut.kr@outlook.co.th>
2023-12-05 18:39:58 +0700

    add service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,15 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+// นำเข้า Injectable จาก Angular core
 import { Injectable } from '@angular/core';
 
+// ใช้ Injectable decorator เพื่อทำให้ class เป็น Angular service และลงทะเบียนเป็น Singleton ใน root injector
 @Injectable({
     providedIn: 'root'
 })
 export class AccountService {
+    // กำหนด baseUrl สำหรับเป็นที่ตั้งของ API ที่ให้บริการ
     baseUrl = 'https://localhost:7777/api/'
 
+    // กำหนด constructor ที่รับ dependency จาก HttpClient
     constructor(private http: HttpClient) { }
 
+    // เมทอด login ใน class AccountService
     login(model: any) {
+        // ใช้ HttpClient ในการทำ HTTP POST request ไปยัง API สำหรับการล็อกอิน
         return this.http.post(`${this.baseUrl}account/login`, model)
     }
 }

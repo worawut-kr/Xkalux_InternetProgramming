b860ce27cdd3b80252d244e890e489deeb1bbfb4
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 20:19:09 +0700

    Added database management and Setup functions.

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure CORS
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
@@ -28,4 +28,26 @@ app.UseAuthorization();
 // Map controllers
 app.MapControllers();
 
+// Run the application
 app.Run();
+
+// Setup the database
+SetupDatabase(app);
+
+void SetupDatabase(IHost app)
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var dataContext = services.GetRequiredService<DataContext>();
+        dataContext.Database.Migrate();
+        Seed.SeedUsers(dataContext).Wait();
+    }
+    catch (Exception e)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(e, "An error occurred during migration");
+    }
+}
+

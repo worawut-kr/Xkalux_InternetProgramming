b68bceca697bece8cb29d14f6a05273d798f39e1
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 20:31:19 +0700

    update usercontroller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,8 +5,6 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-//[ApiController]
-//[Route("api/[controller]")]
 [Authorize]
 public class UsersController : BaseApiController
 {

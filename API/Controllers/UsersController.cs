b9eec0d0dd59caf46f4156df2cc460f731d55b35
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 21:49:30 +0700

    update UserController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,35 +1,43 @@
-﻿namespace API.Controllers;
-
-using API.Entities;
-using API.Interfaces;
+﻿using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
 
+namespace API.Controllers;
 [Authorize]
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        return Ok(await _userRepository.GetUsersAsync());
+        var users = await _userRepository.GetUsersAsync();
+        return Ok(_mapper.Map<IEnumerable<MemberDto>>(users));
     }
 
-    [HttpGet("{id}")] //endpoint: /api/users/25  
-    public async Task<ActionResult<AppUser?>> GetUser(int id)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<MemberDto?>> GetUser(int id)
     {
-        return await _userRepository.GetUserByIdAsync(id);
+        var user = await _userRepository.GetUserByIdAsync(id);
+        var userDto = _mapper.Map<MemberDto?>(user); // Handle nullable mapping
+        return userDto != null ? Ok(userDto) : NotFound();
     }
+
     [HttpGet("username/{username}")]
-    public async Task<ActionResult<AppUser?>> GetUserByUserName(string username)
+    public async Task<ActionResult<MemberDto?>> GetUserByUserName(string username)
     {
-        return await _userRepository.GetUserByUserNameAsync(username);
+        var user = await _userRepository.GetUserByUserNameAsync(username);
+        var userDto = _mapper.Map<MemberDto?>(user); // Handle nullable mapping
+        return userDto != null ? Ok(userDto) : NotFound();
     }
 }
+

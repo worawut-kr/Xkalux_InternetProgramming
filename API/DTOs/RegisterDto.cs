d603d8e8dbc30ac6574ea8688839607146105c1f
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 16:39:43 +0700

    Refactor RegisterDto structure for better validation handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -2,12 +2,13 @@
 
 namespace API.DTOs;
 
-#nullable disable
 public class RegisterDto
 {
     [Required(ErrorMessage = "Please enter a username")]
     [MinLength(3, ErrorMessage = "Please enter a username at least 3 characters")]
-    public string Username { get; set; }
- [MinLength(3, ErrorMessage = "Please enter a PW at least 3 characters")]
-    public string Password { get; set; }
+    public string? Username { get; set; }
+
+    [Required(ErrorMessage = "Please enter a password")]
+    [StringLength(8, MinimumLength = 4)]
+    public string? Password { get; set; }
 }

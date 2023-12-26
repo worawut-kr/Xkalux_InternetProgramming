e5725f1938e4e9880751233fe2d912be349ec636
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 21:14:45 +0700

    Initial AutoMapper configuration for User and Photo entities.

---
+++ API/Helpers/AutoMapperUserProfiles.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using AutoMapper;
+
+namespace API;
+
+public class AutoMapperUserProfiles : Profile
+{
+    public AutoMapperUserProfiles()
+    {
+        CreateMap<AppUser, MemberDto>();
+        CreateMap<Photo, PhotoDto>();
+    }
+}

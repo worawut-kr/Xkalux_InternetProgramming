682a054f975cf38405550ba745c54a8dde1f5658
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 18:38:04 +0700

    Enhance constructor to extract and assign errors from navigation

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-error.component.css']
 })
 export class ServerErrorComponent {
+  err: any
 
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation()
+    this.err = navigation?.extras?.state?.['errors']
+  }
 }

f98e024d0ea16053fe366284c4c6b33720bf6ae9
worawut-kr <117725316+worawut-kr@users.noreply.github.com>
2023-12-26 20:29:41 +0700

    Add IUserRepository Interface

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+
+namespace API.Interfaces;
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+    Task<bool> SaveAllAsync();
+    Task<AppUser?> GetUserByIdAsync(int id);
+    Task<AppUser?> GetUserByUserNameAsync(string username);
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+}
